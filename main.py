fdf34627c35c0daed8d730c45bba13cd4763cad5
ricaportela <ricaportela@gmail.com>
2018-05-07 16:50:53 -0300

    First Commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import db_config, db_connection
 db = db_connection.OracleDB(db_config.user, db_config.passwd, db_config.server, db_config.port, db_config.sid)
 db.connect()
 
-cursor = db.cursor.execute("SELECT * FROM segmentos")
+cursor = db.cursor.execute("SELECT * FROM permissao")
 for result in cursor:
     print(result)
 db.close()
