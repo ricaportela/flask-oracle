9e1b8ce0d61c9183eb6ca94bc83775154b9fc37d
ricaportela <ricaportela@gmail.com>
2018-05-22 16:20:35 -0300

    Working hard to create db connection

--- database.py
+++ database.py
@@ -19,11 +19,10 @@ class OracleDB:
         self.sid = config('DB_SID')
         self.engine = None
         self.conn = None
-        self.rconn = None
-        self.oracle_connection_string = None
-        # self.oracle_connection_string = ('oracle+cx_oracle://{self.username}:{self.password}@' +
-        #     cx_Oracle.makedsn('{self.hostname}', '{self.port}', service_name='{self.sid}')
-        # )
+        self.rconn = None        
+        self.oracle_connection_string = ('oracle+cx_oracle://{username}:{password}@' +
+            cx_Oracle.makedsn('{hostname}', '{port}', service_name='{service_name}')
+        )
 
     def connect(self):
         try:
@@ -37,6 +36,7 @@ class OracleDB:
                 ), pool_size=20)
             self.conn = self.engine.connect()
             print("conectou...")
+
         except cx_Oracle.DatabaseError as e:
             self.engine = None
             print(e)
@@ -47,13 +47,6 @@ class OracleDB:
         for result in result:
             print(result)
 
-    def cameras(self):
-        rconn = self.engine.raw_connection()
-        cur = rconn.connection.cursor()
-        l_cur = cur.var(cx_Oracle.CURSOR)
-        res = cur.callproc('PKG_CAMERA.PRC_SELECT_CAMERA_QTY_BY_CLIENT', (701, l_cur))
-        res = l_cur.getvalue().fetchall()
-        print(res[0][1])
 
     def connection_close(self):
         self.conn.close()
@@ -63,32 +56,5 @@ class OracleDB:
 if __name__ == '__main__':
     ora = OracleDB()
     ora.connect()
-    ora.cameras()
+    ora.resultado()
     ora.connection_close()
-
-
-
-
-
-
-
-
-# from sqlalchemy import create_engine
-# import cx_Oracle
-# from decouple import config
-
-
-# try:
-#     engine = create_engine('oracle+cx_oracle://desenv:devquality@192.168.1.10/orcldev', pool_size=100)
-#     conn = engine.connect()
-#     rconn = engine.raw_connection()
-#     cur = rconn.connection.cursor()
-#     l_cur = cur.var(cx_Oracle.CURSOR)
-#     res = cur.callproc('PKG_CAMERA.PRC_SELECT_CAMERA_QTY_BY_CLIENT', (701, l_cur))
-#     res = l_cur.getvalue().fetchall()
-#     print(res[0][1])
-# except Error as e:
-#     print(e)
-# finally:
-#     rconn.close()
-#     conn.close()
